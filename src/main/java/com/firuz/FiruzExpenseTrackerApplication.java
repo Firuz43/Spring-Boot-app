465041629c95a3cc229e836dfbb23c6e3628c297
Firuz43 <firuz1199@mail.ru>
2022-04-29 17:54:44 +0400

    initializing app

---
+++ src/main/java/com/firuz/FiruzExpenseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.firuz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiruzExpenseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiruzExpenseTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firuz/FiruzExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firuz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiruzExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
